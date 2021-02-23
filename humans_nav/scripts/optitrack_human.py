4ac0a96c629bfaadf3f3d32a04c631996a9699f4
phani <thedecipher.sp@gmail.com>
2021-02-23 11:05:10 +0100

    changes

--- humans_nav/scripts/optitrack_human.py
+++ humans_nav/scripts/optitrack_human.py
@@ -18,13 +18,15 @@ from humans_msgs.msg import HumanMarker
 class OptiTrackHuman():
 
     def __init__(self):
-        
+
         self.pos = Pose()
+        self.prev_pos = Pose()
+        self.prev_pos.orientation.w = 1.0
         self.vel = Twist()
         self.first = True
         self.humans = TrackedHumans()
         self.uwds_humans = HumanArray()
-        
+
         for i in range(1):
             human_segment = TrackedSegment()
             human_segment.type = TrackedSegmentType.TORSO
@@ -38,18 +40,18 @@ class OptiTrackHuman():
             self.uwds_humans.humans.append(humanmarker)
 
 
-    def OptiTrackCB(self, msg): 
+    def OptiTrackCB(self, msg):
         if(msg.pos):
             self.pos.position.x = copy.copy(msg.pos[0].x)+6.4868
             self.pos.position.y = copy.copy(msg.pos[0].y)+2.8506
             self.pos.position.z = copy.copy(msg.pos[0].z)
 
-            self.pos.orientation.x = 0#copy.copy(msg.att[0].qx)
-            self.pos.orientation.y = 0#copy.copy(msg.att[0].qy)
+            self.pos.orientation.x = copy.copy(msg.att[0].qx)
+            self.pos.orientation.y = copy.copy(msg.att[0].qy)
             self.pos.orientation.z = copy.copy(msg.att[0].qz)
             self.pos.orientation.w = copy.copy(msg.att[0].qw)
 
-            if self.first: 
+            if self.first:
                 self.first=False
                 self.prev_pos = copy.deepcopy(self.pos)
 
@@ -69,7 +71,42 @@ class OptiTrackHuman():
                 self.vel.angular.x = (r - rp)/time
                 self.vel.angular.y = (p - pp)/time
                 self.vel.angular.z = (y - yp)/time
-        
+
+
+            self.last_time = now
+            self.prev_pos = copy.deepcopy(self.pos)
+
+        else:
+            self.pos.position.x = self.prev_pos.position.x
+            self.pos.position.y = self.prev_pos.position.y
+            self.pos.position.z = self.prev_pos.position.z
+
+            self.pos.orientation.x = self.prev_pos.orientation.x
+            self.pos.orientation.y = self.prev_pos.orientation.y
+            self.pos.orientation.z = self.prev_pos.orientation.z
+            self.pos.orientation.w = self.prev_pos.orientation.w
+
+            if self.first:
+                self.first=False
+                self.prev_pos = copy.deepcopy(self.pos)
+
+            now = rospy.Time.now()
+            time = (now-self.last_time).to_nsec()/(10.0**9)
+            if time>=0.0:
+                self.vel.linear.x = (self.pos.position.x - self.prev_pos.position.x)/time
+                self.vel.linear.y = (self.pos.position.y - self.prev_pos.position.y)/time
+                self.vel.linear.z = (self.pos.position.z - self.prev_pos.position.z)/time
+
+                quat = (self.prev_pos.orientation.x,self.prev_pos.orientation.y,self.prev_pos.orientation.z,self.prev_pos.orientation.w)
+                r,p,y = euler = euler_from_quaternion(quat)
+
+                quat_prev = (self.prev_pos.orientation.x,self.prev_pos.orientation.y,self.prev_pos.orientation.z,self.prev_pos.orientation.w)
+                rp,pp,yp = euler = euler_from_quaternion(quat_prev)
+
+                self.vel.angular.x = (r - rp)/time
+                self.vel.angular.y = (p - pp)/time
+                self.vel.angular.z = (y - yp)/time
+
 
             self.last_time = now
             self.prev_pos = copy.deepcopy(self.pos)
@@ -87,7 +124,7 @@ class OptiTrackHuman():
         self.publishHumans()
 
     def publishHumans(self):
-        
+
         if(self.humans.humans):
             self.humans.header.stamp = rospy.Time.now()
             self.humans.header.frame_id = 'map'
@@ -97,13 +134,14 @@ class OptiTrackHuman():
             #print(self.humans)
 
     def run_and_publish(self):
-        
+
         rospy.init_node('mocap_humans')
         rospy.Subscriber('/optitrack/bodies/Rigid_Body_1',or_pose_estimator_state,self.OptiTrackCB)
         #rospy.Subscriber('/optitrack/bodies/Helmet_3',or_pose_estimator_state,self.OptiTrackCB)
         self.pub = rospy.Publisher('/tracked_humans', TrackedHumans, queue_size=10)
         self.uwds_pub = rospy.Publisher('/humans', HumanArray, queue_size=1)
         self.last_time = rospy.Time.now()
+        rospy.Rate(10)
         rospy.spin()
 
 a = OptiTrackHuman()
